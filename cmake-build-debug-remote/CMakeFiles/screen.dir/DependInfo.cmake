64ac34beef93d47a9d1dc516f1da03198025c73b
warriortweety <oswald.toma@gmail.com>
2019-10-12 18:48:15 +0200

    added .gitignore, and cmake-build-debug-remote directory (just in case)

---
+++ cmake-build-debug-remote/CMakeFiles/screen.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/tmp.WFhMyKFq16/driver/sh1106.c" "/tmp/tmp.WFhMyKFq16/cmake-build-debug-remote/CMakeFiles/screen.dir/driver/sh1106.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/tmp.WFhMyKFq16/CButtons.cpp" "/tmp/tmp.WFhMyKFq16/cmake-build-debug-remote/CMakeFiles/screen.dir/CButtons.cpp.o"
+  "/tmp/tmp.WFhMyKFq16/CController.cpp" "/tmp/tmp.WFhMyKFq16/cmake-build-debug-remote/CMakeFiles/screen.dir/CController.cpp.o"
+  "/tmp/tmp.WFhMyKFq16/CMainScreen.cpp" "/tmp/tmp.WFhMyKFq16/cmake-build-debug-remote/CMakeFiles/screen.dir/CMainScreen.cpp.o"
+  "/tmp/tmp.WFhMyKFq16/main.cpp" "/tmp/tmp.WFhMyKFq16/cmake-build-debug-remote/CMakeFiles/screen.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
